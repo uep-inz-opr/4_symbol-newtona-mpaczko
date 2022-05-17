da0650956719a8451e05d47e00322c368ee40c1f
Michal <michalpaczko@gmail.com>
2022-05-17 09:17:40 +0200

    test3

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-from re import S
 
 input = input()
 
@@ -8,20 +7,8 @@ inputTrim = input.replace(" ", "")
 map = map(int, inputTrim)
 list = list(map)
 
-x = list[0]
-y = list[1]
+n = list[0]
+k = list[1]
 
-if y == x:
-    print(1)
-elif y == 1:         # see georg's comment
-    print(x)
-elif y > x:          # will be executed only if y != 1 and y != x
-    print(0)
-else:                # will be executed only if y != 1 and y != x and x <= y
-    a = math.factorial(x)
-    b = math.factorial(y)
-    c = math.factorial(x-y)  # that appears to be useful to get the correct result
-    div = a // (b * c)
-    print(div) 
-
+print(math.comb(n,k))
 
